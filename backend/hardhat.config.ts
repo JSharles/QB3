57f188d3165c03fba6ea732bc39a930174d07ca1
JSharles <jeancharlesbarq@gmail.com>
2025-04-04 23:27:20 +0200

    chore: setup sepolia deployment

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,20 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+      chainId: 11155111,
+    },
+    hardhat: {},
+  },
 };
 
 export default config;
