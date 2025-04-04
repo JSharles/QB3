a879ac8d3990315c0e68e1d7622ed980f67f2d3b
JSharles <jeancharlesbarq@gmail.com>
2025-04-04 11:04:36 +0200

    fix: set contract as minter + add toast

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,9 +1,15 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { hardhat } from "wagmi/chains";
+import { createPublicClient, http } from "viem";
 
 export const config = getDefaultConfig({
   appName: "QB3",
   projectId: "YOUR_PROJECT_ID",
   chains: [hardhat],
   ssr: true,
+});
+
+export const publicClient = createPublicClient({
+  chain: hardhat,
+  transport: http(),
 });
